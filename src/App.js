39ffc92944b7dddad8b8e9ef7d732deef145a861
rodrigo.lavratti <rodrigo.l.lavratti@gmail.com>
2023-03-27 17:07:03 -0300

    Filtro repositorio gitHub

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [repos, setRepos] = useState([]);
+  const [search, setSearch] = useState('');
+
+  //console.log('Renderizou')
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/rodrigolavratti/repos')
+      .then(response => response.json())
+      .then(data => setRepos(data))
+  }, [])
+
+  const filteredRepos = search.length > 0 
+    ? repos.filter(repo => repo.name.includes(search))
+    : [];
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input 
+        type="text" 
+        name="search" 
+        placeholder='Buscar...' 
+        onChange={e => setSearch(e.target.value)}
+        value={search}
+      />
+
+      {search.length > 0 ? (
+          <ul>
+          {filteredRepos.map(repo => {
+            return (
+              <li key={repo.name}>
+                {repo.name}
+              </li>
+            )
+          })}
+        </ul>
+      ) : (
+        <ul>
+          {repos.map(repo => {
+            return (
+              <li key={repo.name}>
+                {repo.name}
+              </li>
+            )
+          })}
+        </ul>
+      )}
     </div>
   );
 }
